d1c2f5aa7cbd470464bc2b1455a3d8f50309aa14
Varsha Rao <vrakinep@ncsu.edu>
2021-11-03 00:25:07 -0400

    added wallet_table

--- SQL files/Create_tables.sql
+++ SQL files/Create_tables.sql
@@ -12,3 +12,9 @@ user_type varchar2(200) NOT NULL)
 select table_name from user_tables;
 select * from admin;
 select * from market_place;
+
+create table wallet_table(
+wallet_id varchar2(200),
+lyl_prgm_code varchar2(200),
+points_acc varchar2(200),
+primary key(wallet_id,lyl_prgm_code))
