6ef88275cff160aa3699d7a86cabae953f7246f0
pvemuri <pvemuri@ncsu.edu>
2021-11-07 18:36:47 -0500

    commit 9
    Added a template landing page for Show Queries.

--- Codebase/showQueries.java
+++ Codebase/showQueries.java
@@ -25,42 +25,44 @@ public class showQueries {
         try {
             do{
                 System.out.println("\t\tADMIN LANDING PAGE\n\n");
-                System.out.println("1. Add Brand");
-                System.out.println("2. Add Customer");
-                System.out.println("3. Show Brand's Info");
-                System.out.println("4. Show Customer's Info");
-                System.out.println("5. Add activity type");
-                System.out.println("6. Add reward type");
-                System.out.println("7. Logout");
+                System.out.println("1. List all customers that are not part of Brand02’s program.");
+                System.out.println("2. List customers that have joined a loyalty program but have not participated in any activity in that program (list the customerid and the loyalty program id).");
+                System.out.println("3. List the rewards that are part of Brand01 loyalty program.");
+                System.out.println("4. List all the loyalty programs that include “refer a friend” as an activity in at least one of their reward rules.");
+                System.out.println("5. For Brand01, list for each activity type in their loyalty program, the number instances that have occurred.");
+                System.out.println("6. List customers of Brand01 that have redeemed at least twice.");
+                System.out.println("7. All brands where total number of points redeemed overall is less than 500 points");
+                System.out.println("8. For Customer C0003, and Brand02, number of activities they have done in the period of 08/1/2021 and 9/30/2021.");
+                System.out.println("9. Go Back.");
                 selection = sc.nextInt();
                 sc.nextLine();
                 switch (selection) {
                 case 1:
-                    System.out.println("Adding Brand.....");
-                    BrandSignUpUI(conn);
+                    System.out.println("List all customers that are not part of Brand02’s program.");
                     break;
                 case 2:
-                    System.out.println("Adding Customer.....");
-                    CustomerSignUpUI(conn);
+                    System.out.println("List customers that have joined a loyalty program but have not participated in any activity in that program.");
                     break;
                 case 3:
-                    System.out.println("Showing Brand's Info.....");
-                    ShowInfo.ShowBrandInfo(conn);
+                    System.out.println("List the rewards that are part of Brand01 loyalty program.");
                     break;
                 case 4:
-                    System.out.println("Showing Customer's Info.......");
-                    ShowInfo.ShowCustomerInfo(conn);
+                    System.out.println("List all the loyalty programs that include “refer a friend” as an activity in at least one of their reward rules.");
                     break;
                 case 5:
-                    System.out.println("Adding activity type.........");
-                    AddInfo.addActivityType(conn);
+                    System.out.println("For Brand01, list for each activity type in their loyalty program, the number instances that have occurred.");
                     break;
                 case 6:
-                    System.out.println("Adding reward type.......");
-                    AddInfo.addRewardType(conn);
+                    System.out.println("List customers of Brand01 that have redeemed at least twice.");
                     break;
                 case 7:
-                    System.out.println("Logging out....");
+                    System.out.println("All brands where total number of points redeemed overall is less than 500 points");
+                    break;   
+                case 8:
+                    System.out.println("For Customer C0003, and Brand02, number of activities they have done in the period of 08/1/2021 and 9/30/2021.");     
+                    break;
+                case 9:
+                    System.out.println("Going back....");
                     main_flag = false;
                     break;    
                 default:
@@ -76,6 +78,15 @@ public class showQueries {
         System.out.println("Failed to connect to DB");
         e.printStackTrace();
     }
+    }
+
+    static void close(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (Throwable whatever) {
+            }
+        }
     }
 
 }
